4011d6aae4c83c550faba469d381e2f3756cc892
Diego Kremer <diego.kremer@gmail.com>
2017-10-11 21:56:17 -0300

    Criado três rotas teste

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("App Express será criado aqui!")
+var express = require ("express"); // Inclui conteúdo do módulo Express
+var app = express(); // Executa Express e salva na variável
+
+//Rotas:
+// "/" => "Olá!"
+app.get("/", function(req, res){
+    res.send("Olá!")
+});
+
+
+// "/tchau" => "Tchau!"
+app.get("/tchau", function(req, res){
+    res.send("Tchau!")
+});
+// "/cachorro" => "MEOW!!"
+app.get("/cachorro", function(req, res){
+    res.send("MEOW!!")
+});
+
+// Indica para o Express começar a escutar as requisições (Inicia servidor)
+app.listen(3000, function(){
+    console.log("Servidor iniciado.")
+});
