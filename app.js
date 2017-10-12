a456b0f6adb9b9690bc4f9b94da46793bab0268a
Diego Kremer <diego.kremer@gmail.com>
2017-10-11 22:39:59 -0300

    Rotas com parametros

--- app.js
+++ app.js
@@ -17,6 +17,24 @@ app.get("/cachorro", function(req, res){
     res.send("MEOW!!")
 });
 
+// Padrões de caminhos - Segue um padrão de rota com variáveis
+app.get("/r/:subredditNome", function(req, res){
+    console.log(req.params);
+    var subreddit = req.params.subredditNome;
+    res.send("Bem vindo ao subreddit do " + subreddit)
+});
+
+// Padrões de caminhos - Segue um padrão de rota com variáveis
+app.get("/r/:subredditNome/comments/:id/:titulo", function(req, res){
+    console.log(req.params);
+    res.send("Bem vindo a página de comentários") 
+});
+
+// Mostra erro caso usuário não digite uma página existente
+app.get("*", function(req, res){
+    res.send("Página não encontrada!")
+});
+
 // Indica para o Express começar a escutar as requisições (Inicia servidor)
 app.listen(3000, function(){
     console.log("Servidor iniciado.")
